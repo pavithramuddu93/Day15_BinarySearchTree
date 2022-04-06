0f740097f4829226944e569693dfcdb3a9f1b483
pavithramuddu93 <pavithramuddu93>
2022-04-06 13:15:00 +0530

    UC2 added

--- src/com/blz/MYBinaryTreeTest.java
+++ src/com/blz/MYBinaryTreeTest.java
@@ -6,6 +6,17 @@ public class MYBinaryTreeTest {
         myBinaryTree.add(56);
         myBinaryTree.add(30);
         myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(11);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(65);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+
         int size = myBinaryTree.getSize();
         System.out.println("Size is : "+size);
 
